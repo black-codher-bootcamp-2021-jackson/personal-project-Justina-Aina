f215348f5c40dedc07344346a6ba242c3147d334
J Aina <j.aina3.14159@outlook.com>
2022-02-13 18:45:05 +0000

    began components - with user detail component

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,74 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { getAllUserData, createUser, deleteUser } from "./services/journeyService";
+// import { getAllUserData, createUser, deleteUser } from "./services/journeyService";
+
+import userProfileDetails from "./components/UserProfileDetails";
 
 function App() {
 
-    const [users, setUsers] = useState(null);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<userProfileDetails/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 
-    useEffect(() => {
-        async function getUsers() {
-          if (!users) {
-            const response = await getAllUserData();
-            // await createUser({first_name: "Jane"});
-            // await deleteUser("620559616af2efcfd6eada69");
-            setUsers(response);
-          }
-        }
+    // const [users, setUsers] = useState(null);
+
+    // useEffect(() => {
+    //     async function getUsers() {
+    //       if (!users) {
+    //         const response = await getAllUserData();
+    //         // await createUser({first_name: "Jane"});
+    //         // await deleteUser("620559616af2efcfd6eada69");
+    //         setUsers(response);
+    //       }
+    //     }
     
-        getUsers();
-    }, [users]);
+    //     getUsers();
+    // }, [users]);
 
-    const renderUsers = (profile) => {
-        console.log(profile);
+    // const renderUsers = (profile) => {
+    //     console.log(profile);
 
-        return (
-          <li key={profile._id}>
-            <h3>
-              {`${profile.first_name} 
-              ${profile.last_name}`}
-            </h3>
-            <p>{`${profile.birthday}`}</p>
-            <p>Wants</p>
-            <ol>
-              <li>{`${profile.wants[1]}`}</li>
-              <li>{`${profile.wants[2]}`}</li>
-              <li>{`${profile.wants[3]}`}</li>
-              <li>{`${profile.wants[4]}`}</li>
-            </ol>
-            <p>Don't want</p>            
-            <ol>
-              <li>{`${profile.dont_wants[1]}`}</li>
-              <li>{`${profile.dont_wants[2]}`}</li>
-              <li>{`${profile.dont_wants[3]}`}</li>
-              <li>{`${profile.dont_wants[4]}`}</li>
-            </ol>        
-          </li>
-        );
-    };
+    //     return (
+    //       <li key={profile._id}>
+    //         <h3>
+    //           {`${profile.first_name} 
+    //           ${profile.last_name}`}
+    //         </h3>
+    //         <p>{`${profile.birthday}`}</p>
+    //         <p>Wants</p>
+    //         <ol>
+    //           <li>{`${profile.wants[1]}`}</li>
+    //           <li>{`${profile.wants[2]}`}</li>
+    //           <li>{`${profile.wants[3]}`}</li>
+    //           <li>{`${profile.wants[4]}`}</li>
+    //         </ol>
+    //         <p>Don't want</p>            
+    //         <ol>
+    //           <li>{`${profile.dont_wants[1]}`}</li>
+    //           <li>{`${profile.dont_wants[2]}`}</li>
+    //           <li>{`${profile.dont_wants[3]}`}</li>
+    //           <li>{`${profile.dont_wants[4]}`}</li>
+    //         </ol>        
+    //       </li>
+    //     );
+    // };
 
-    return (
-        <div>
-          <ul>
-            {users && users.length > 0 ? (
-              users.map((user) => renderUsers(user))
-            ) : (
-              <p>No user found</p>
-            )}
-          </ul>
-        </div>
-      );
+    // return (
+    //     <div>
+    //       <ul>
+    //         {users && users.length > 0 ? (
+    //           users.map((user) => renderUsers(user))
+    //         ) : (
+    //           <p>No user found</p>
+    //         )}
+    //       </ul>
+    //     </div>
+    //   );
 
 }
 
---
+++ client/src/components/UserProfileDetails.js
@@ -0,0 +1,189 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+//import '../App.css';
+
+import { getAllUserData } from "./services/journeyService";
+
+const userProfileDetails = () => {
+    
+  const [users, setUsers] = useState(null);
+    
+  // Line 12 - Calling getAllUserData from journeyService
+  useEffect(() => {
+    async function getUsers() {
+      if (!users) {
+        const response = await getAllUserData();
+        
+        setUsers(response);
+      }
+    }
+
+    getUsers();
+  }, [users]);
+
+  const renderUsers = (profile) => {
+    console.log(profile);
+
+    return (
+      
+      <section id='UserProfileDetails' key={profile._id} >
+
+        <h3 className='User-Name'>{`${profile.first_name} ${profile.last_name}`}</h3>
+        
+        <div className='User-Birthday'>
+          <p>{`${profile.birthday}`}</p>
+        </div>
+          
+        <div>
+          <h4 className='User-Wants-Title'>Wants</h4>
+          <ol className='User-Wants-Detail-box'>
+            <li className='User-Wants-Details'>{`${profile.wants[1]}`}</li>
+            <li className='User-Wants-Details'>{`${profile.wants[2]}`}</li>
+            <li className='User-Wants-Details'>{`${profile.wants[3]}`}</li>
+            <li className='User-Wants-Details'>{`${profile.wants[4]}`}</li>
+          </ol>
+        </div>
+
+        <div>
+          <h4 className='User-DontWants-Title'>Not Wanted</h4>
+          <ol className='User-DontWants-Detail-box'>
+            <li className='User-DontWants-Details'>{`${profile.dont_wants[1]}`}</li>
+            <li className='User-DontWants-Details'>{`${profile.dont_wants[2]}`}</li>
+            <li className='User-DontWants-Details'>{`${profile.dont_wants[3]}`}</li>
+            <li className='User-DontWants-Details'>{`${profile.dont_wants[4]}`}</li>
+          </ol>
+        </div>
+
+      </section>
+      
+    );
+  };
+
+  return (
+    <div>
+      <ul>
+        {users && users.length > 0 ? (
+          users.map((user) => renderUsers(user))
+        ) : (
+          <p>No user found</p>
+        )}
+      </ul>
+    </div>
+  );
+
+},
+
+export default userProfileDetails;
+
+
+// // app.delete("/rhymes/:id", (req, res) => {
+// //   const id = req.params.id;
+// //   const updatedRhymes = rhymes.filter((rhyme) => rhyme.id != id);
+
+// //   fs.writeFileSync(rhymesAbsoluteFilePath, JSON.stringify(updatedRhymes));
+
+// //   res.status(202).end();
+// // });
+
+// const PersonDetails = () => {
+//   const {id} = useParams();
+//   let [person, setPerson] = useState();
+
+//   const fetchPerson = useCallback(async () => {
+//     let response = await fetch('http://localhost:8080/api/people/' + id)
+//     response = await response.json();
+//     setPerson(response);
+// },[id]);
+
+//   useEffect(() => {
+//     fetchPerson();
+//   }, [fetchPerson]);
+
+//   function onDeleteClick (id) {
+//     fetch('http://localhost:8082/api/books/'+id, {
+//       method: "DELETE"
+//     })
+//       .then(res => {
+//         this.props.history.push("/");
+//       })
+//       .catch(err => {
+//         console.log("Error form PersonDetail_deleteClick");
+//       })
+//   };
+
+//     return (
+//       <div className="PersonDetail">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-md-10 m-auto">
+//               <br /> <br />
+//               <Link to="/" className="btn btn-outline-warning btn-lg float-left">
+//                   Person List
+//               </Link>
+//             </div>
+//             <br />
+//             <div className="col-md-8 m-auto">
+//               <h1 className="display-4 text-center">Person's Record</h1>
+//               <p className="lead text-center">
+//                   View Person's Info
+//               </p>
+//               <hr /> <br />
+//             </div>
+//           </div>
+//           <div>
+//           <div>
+//     <table className="table table-hover table-dark">
+//     {/* <thead>
+//       <tr>
+//         <th scope="col">#</th>
+//         <th scope="col">First</th>
+//         <th scope="col">Last</th>
+//         <th scope="col">Handle</th>
+//       </tr>
+//     </thead> */}
+//     <tbody>
+//       <tr>
+//         <th scope="row">1</th>
+//         <td>First Name</td>
+//         <td>{ person?.name.first }</td>
+//       </tr>
+//           <tr>
+//             <th scope="row">2</th>
+//             <td>Last Name</td>
+//             <td>{ person?.name.last }</td>
+//           </tr>
+//           <tr>
+//             <th scope="row">3</th>
+//             <td>Registration Date</td>
+//             <td>{ person?.registered.date }</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//           </div>
+
+//           <div className="row">
+//             <div className="col-md-6">
+//               <button type="button" className="btn btn-outline-danger btn-lg btn-block" onClick={() => onDeleteClick(person?._id)}>Delete Person</button><br />
+//             </div>
+
+//             {/* <div className="col-md-6">
+//               <Link to={`/edit-person/${person?._id}`} className="btn btn-outline-info btn-lg btn-block">
+//                     Edit Person
+//               </Link>
+//               <br />
+//             </div> */}
+
+//           </div>
+//             {/* <br />
+//             <button type="button" class="btn btn-outline-info btn-lg btn-block">Edit Person</button>
+//             <button type="button" class="btn btn-outline-danger btn-lg btn-block">Delete Person</button> */}
+//         </div>
+//       </div>
+//   );
+// }
+
+// export default PersonDetails;
+
+
+
